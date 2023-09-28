7bff61ea3b4ad538b5681ee2fa604b200ed26675
Damitha <hellodamitha@gmail.com>
2023-09-28 18:44:31 +0530

    list added

--- src/body.jsx
+++ src/body.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 const Body = () => {
-  return <div className="Body">Body</div>;
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "one half pound bag of cocoa",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "bread",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "pizza",
+    },
+  ]);
+
+  return (
+    <div className="Body">
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked} />
+            <label>{item.item}</label>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Body;

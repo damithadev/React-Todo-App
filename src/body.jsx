26d5d5a6c732379a212e67794e6fbe49b7a55eed
Damitha <hellodamitha@gmail.com>
2023-09-28 21:57:08 +0530

    ItemList props updated

--- src/body.jsx
+++ src/body.jsx
@@ -1,32 +1,16 @@
 import React from "react";
-import { FaTrashAlt } from "react-icons/fa";
+
+import ItemList from "./ItemList";
 
 const Body = ({ items, handleCheck, handleDelete }) => {
   return (
     <div className="Body">
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                checked={item.checked}
-                onChange={() => handleCheck(item.id)}
-              />
-              <label
-                style={item.checked ? { textDecoration: "line-through" } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <FaTrashAlt
-                role="button"
-                tabIndex="0"
-                onClick={() => handleDelete(item.id)}
-              />
-            </li>
-          ))}
-        </ul>
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p
           style={{

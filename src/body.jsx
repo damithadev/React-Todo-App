55bb3ac0b07087658a054ad82b83e09f8d1ea017
Damitha <hellodamitha@gmail.com>
2023-09-28 18:54:14 +0530

    css added for list

--- src/body.jsx
+++ src/body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Body = () => {
   const [items, setItems] = useState([
@@ -27,7 +28,7 @@ const Body = () => {
           <li className="item" key={item.id}>
             <input type="checkbox" checked={item.checked} />
             <label>{item.item}</label>
-            <button>Delete</button>
+            <FaTrashAlt role="button" tabIndex="0" />
           </li>
         ))}
       </ul>
